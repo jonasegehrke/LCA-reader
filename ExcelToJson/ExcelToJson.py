1eca5a0006414fc0bccf520b5f2be8a4b5792325
AndreasHolmAndersen <andreas.holm.andersen@gmail.com>
2022-09-09 10:20:54 +0200

    finished tabel7 in right format

--- ExcelToJson/ExcelToJson.py
+++ ExcelToJson/ExcelToJson.py
@@ -1,41 +1,94 @@
 import json
+from itertools import chain, islice, tee
+from re import T
 
 import pandas as pd
 
+
+def previous_and_next(some_iterable):
+    prevs, items, nexts = tee(some_iterable, 3)
+    prevs = chain([None], prevs)
+    nexts = chain(islice(nexts, 1, None), [None])
+    return zip(prevs, items, nexts)
 
 
 data = pd.read_excel('tabel-7.xlsx', sheet_name='Bilag 2, tabel 7 renset')
 
 json_data = data.to_json(orient='records')
 
-
-results = json.loads(json_data)
-
-new_results = []
-new_result = {
-  "Indikatorer": {}
-}
-for key in results:
-    new_result = {}
-    new_result_indikatorer = {}
-
-    new_result["Title"] = key["Navn"]    
-    new_result["Fase"] = key["Fase"]    
-    new_result["Indikatorfaktor"] = str(key["Deklareret faktor"] )   
-    new_result["Indikatorenhed"] = key["Deklareret enhed"]    
-    new_result["Massefaktor"] = str(key["Massefaktor"] ) 
-
-    new_result_indikatorer["GWP"] = str(key["Global Opvarmning"]) + "kg CO\u2082 - eq . / m\u00b2"
-    new_result["Indikatorer"] = new_result_indikatorer
-    new_results.append(new_result)
-
-    
-
-final = json.dumps(new_results, indent=2)
+rawData = json.loads(json_data)
 
 
+def get_names():
+    name_list = []
+    for data in rawData:
+        name_list.append(data["Navn"])
+    return list(dict.fromkeys(name_list))
 
-with open("resultTabel7.json", "w") as outfile:
+
+names_list = get_names()
+
+
+def sort():
+    results = []
+    for name in names_list:
+        stages = []
+        epd = {
+            "stages": stages
+        }
+
+        for data in rawData:
+            if data["Navn"] == name:
+                stage = {}
+                stage["measures"] = {}
+                epd = {
+                    "stages": stages,
+                    "declaredUnit": {},
+                    "epdInfo": {}
+                }
+                if data["Fase"] == "A1tilA3":
+                  stage["stageType"] = 0
+
+                if data["Fase"] == "C3":
+                  stage["stageType"] = 15
+
+                if data["Fase"] == "C4":
+                  stage["stageType"] = 16
+
+                if data["Fase"] == "D":
+                  stage["stageType"] = 17
+
+                stage["stageStatus"] = 2
+                stage["measures"] = {"GWP": data["Global Opvarmning"]}
+                
+                epd["shortName"] = name
+                epd["ownerId"] = None
+                epd["custom"] = False
+                epd["scraped"] = True
+                epd["generic"] = True
+                epd["expectedLifespan"] = None
+                epd["description"] = None
+                epd["link"] = ["https://hoeringsportalen.dk/Hearing/Details/66338", "https://prodstoragehoeringspo.blob.core.windows.net/a25cc0f0-2669-42ea-bc8a-f2f84aca8600/Tabel%207%20-%20Generisk%20datagrundlag%202023.pdf"]
+                epd["additionalSources"] = [""]
+                epd["declaredUnit"]["declaredUnit"] = data["Deklareret enhed"]
+                epd["declaredUnit"]["declaredValue"] = data["Deklareret faktor"]
+                epd["declaredUnit"]["mass"] = data["Massefaktor"]
+                epd["declaredUnit"]["massUnit"] = None
+                epd["epdInfo"]["epdSpecificationForm"] = 0
+                epd["epdInfo"]["epdProductIndustryType"] = None
+                epd["epdInfo"]["issuedAt"] = None
+                epd["epdInfo"]["validTo"] = None
+                epd["tags"] = [""]
+                epd["stages"].append(stage)
+        results.append(epd)
+    return results
+
+
+results = sort()
+
+
+final = json.dumps(results, indent=2)
+
+
+with open("result-tabel-7.json", "w") as outfile:
     outfile.write(final)
-
-
